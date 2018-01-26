0f7e14c87c7930221bf1e92f7efcff864c73b80e
root <lya1b2333@hotmail.com>
2018-01-26 10:53:09 +0800

    python_update

---
+++ python_update/python.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+#author : jasonliu
+
+#下载系统依赖
+yum -y install wget sqlite-devel xz gcc automake zlib-devel openssl-devel readline-devel
+
+#下载源码包
+wget http://mirrors.sohu.com/python/3.6.4/Python-3.6.4.tgz
+
+#解压安装
+tar -xvf Python-3.6.4.tgz
+cd Python-3.6.4
+./configure
+make 
+make install
+
+#修改文件指向
+sed -i '1c #!/usr/bin/python2' /usr/bin/yum
+sed -i '1c #!/usr/bin/python2' /usr/libexec/urlgrabber-ext-down
+sed -i '1c #!/usr/bin/python2 -Es' /usr/bin/firewall-cmd
+
+#做一个软连接让新Python可用
+ln -sb /usr/local/bin/python3.6 /usr/bin/python
+
+#测试
+echo "\n当前Python版本：" `python --version`
